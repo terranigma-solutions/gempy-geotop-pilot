5c266687495c233e2440da6c6b85b1f96bb91320
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-09 15:04:29 +0200

    - [ENH/WIP] Plot data and fault in the same figure

--- gempy_geotop_pilot/utils.py
+++ gempy_geotop_pilot/utils.py
@@ -1,6 +1,5 @@
 ﻿import gempy_viewer as gpv
 
-PLOT_3D = True
 
 
 def plot_geotop(geo_model, ve=1, image_3d=True):
@@ -10,17 +9,20 @@ def plot_geotop(geo_model, ve=1, image_3d=True):
                 show_data=True,
                 show_boundaries=False,
                 ve=ve)
-    if PLOT_3D:
-        gempy_plot3d = gpv.plot_3d(
-            model=geo_model,
-            show_data=True,
-            show_lith=False,
-            ve=ve,
-            image=False,
-            kwargs_pyvista_bounds={
-                'show_xlabels': False,
-                'show_ylabels': False,
-                'show_zlabels': False,
-            },
-            kwargs_plot_data={'arrow_size': 100}
-        )
+    
+    gempy_plot3d = gpv.plot_3d(
+        model=geo_model,
+        show_data=True,
+        show_lith=False,
+        ve=ve,
+        image=False,
+        kwargs_pyvista_bounds={
+            'show_xlabels': False,
+            'show_ylabels': False,
+            'show_zlabels': False,
+        },
+        kwargs_plot_data={'arrow_size': 100}
+    )
+    
+    return gempy_plot3d
+    
--- tests/test_visualize_data.py
+++ tests/test_visualize_data.py
@@ -7,6 +7,7 @@ from gempy_geotop_pilot.utils import plot_geotop
 from .test_basic_interpolation import setup_AP_geomodel
 
 import gempy as gp
+import gempy_viewer as gpv
 import subsurface
 
 PLOT_3D = True
@@ -25,7 +26,7 @@ def test_plot_input_AP_only():
 def test_plot_input_all():
     data: pd.DataFrame = read_all_boreholes_data_to_df(path)
     geo_model: gp.data.GeoModel = initialize_geomodel(data)
-    setup_south_model(geo_model, slice(0, -1))
+    setup_south_model(geo_model, slice(None))
     plot_geotop(geo_model)
 
 
@@ -45,3 +46,37 @@ def test_plot_fault_mesh():
         ve=5
     )
 
+
+def test_plot_data_and_fault_data_in_same_plot():
+    data: pd.DataFrame = read_all_boreholes_data_to_df(path)
+    geo_model: gp.data.GeoModel = initialize_geomodel(data)
+    setup_south_model(geo_model, slice(None))
+
+    ver, sim = read_all_fault_data_to_mesh(path_to_faults)
+    unstruct = subsurface.UnstructuredData.from_array(
+        vertex=ver,
+        cells=sim
+    )
+
+    trisurf = subsurface.TriSurf(unstruct)
+    vista_mesh: "pyvista.PolyData" = subsurface.visualization.to_pyvista_mesh(trisurf)
+    
+    gempy_plot3d = gpv.plot_3d(
+        model=geo_model,
+        show_data=True,
+        show_lith=False,
+        ve=10,
+        image=False,
+        kwargs_pyvista_bounds={
+            'show_xlabels': False,
+            'show_ylabels': False,
+            'show_zlabels': False,
+        },
+        kwargs_plot_data={'arrow_size': 100},
+        show=False
+    )
+    
+    
+    gempy_plot3d.p.add_mesh(vista_mesh)
+    gempy_plot3d.p.show()
+    
