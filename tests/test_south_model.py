83def5ea2723068e9c72d273c55359f256b69344
Miguel de la Varga <miguel@terranigma-solutions.com>
2023-11-15 12:01:52 +0100

    - Remove hard torch dependency
    - Tweaking geotop

--- tests/test_south_model.py
+++ tests/test_south_model.py
@@ -43,7 +43,7 @@ def test_south_model_no_faults_only_wells():
     gp.compute_model(
         gempy_model=geo_model,
         engine_config=GemPyEngineConfig(
-            use_gpu=True,
+            use_gpu=False,
             dtype='float32'
         )
     )
@@ -63,9 +63,9 @@ def test_south_model_with_faults():
     all_faults_unstructs: list[subsurface.UnstructuredData] = read_all_fault_data_to_mesh(
         path=config.get('FAULTS_SOUTH_FOLDER')
     )
-    # faults_slicer = all_faults_unstructs[10:13]
+    faults_slicer = all_faults_unstructs[10:13]
 
-    faults_slicer = all_faults_unstructs
+    # faults_slicer = all_faults_unstructs
     for e, struct in enumerate(faults_slicer):
         add_fault_from_unstructured_data(
             unstruct=struct,
@@ -76,7 +76,7 @@ def test_south_model_with_faults():
     gp.compute_model(
         gempy_model=geo_model,
         engine_config=GemPyEngineConfig(
-            use_gpu=True,
+            use_gpu=False,
             dtype='float64'
         )
     )
